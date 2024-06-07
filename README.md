d02e712eb625381a0772b6a8346b186598f97593
Büşra Demi <busratekin5427@gmail.com>
2024-06-08 01:12:42 +0300

    home page and read me güncellendi

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
-# React + Vite
+# VET APP PROJECECT
+## Live Link: https://main--admirable-crostata-462e5a.netlify.app/
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Veterinary Management System is a project created to organize and manage the daily affairs of veterinary clinics. With this project, the veterinary employee is enabled to manage veterinarians, customers, animals and their vaccinations, and appointments.
 
-Currently, two official plugins are available:
+### Technologies
+React
+JavaScript
+HTML
+CSS
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+### Project Design
+This project was prepared with react.
+The project is designed as a one page and is wrapped with the React Router Dom structure.
+CRUID operations were carried out in the project.
+API operations with Axios have been completed.
+Responsive design has been applied.
+A more flexible and functional page design has been implemented with the Flex structure.
+
+### Features included in the project:
+There are 8 pages in the project: home, doctor, customer, animal, vaccination, working day, appointment and report.
+Data retrieval (get), data addition (post), data deletion (data deletion), data update (put) operations were carried out on all pages except Home.
+Searches can be made using the list title fields on the pages.
+The page has a responsive design.
+Page directions are provided by the buttons on the page.
--- src/HomePage/Home.jsx
+++ src/HomePage/Home.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from 'react';
+import { TbVaccine, TbReportMedical } from "react-icons/tb";
+import { MdPeopleAlt, MdPets, MdWorkOutline } from "react-icons/md";
+import { FaUserDoctor } from "react-icons/fa6";
+import { IoCalendarNumberOutline } from "react-icons/io5";
+import './Home.css'
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+const navigate = useNavigate(); 
   return (
-    <div>Home</div>
-  )
+    <div className="home-container">
+      <h1>İŞLEMLER</h1>
+      <div className="icon-grid">
+        <div className="icon-item">
+          <MdPeopleAlt className="icon" onClick={()=> navigate("/customers")}/>
+          <h2>Müşteri</h2>
+        </div>
+        <div className="icon-item">
+          <FaUserDoctor className="icon" 
+          onClick={()=> navigate("/doctors")}/>
+          <h2>Doktor</h2>
+        </div>
+        <div className="icon-item">
+          <MdPets className="icon" onClick={()=> navigate("/animals")}/>
+          <h2>Hayvan</h2>
+        </div>
+        <div className="icon-item">
+          <MdWorkOutline className="icon" onClick={()=> navigate("/doctor-available-dates")}/>
+          <h2>Doktor Çalışma Günü</h2>
+        </div>
+        <div className="icon-item">
+          <IoCalendarNumberOutline className="icon" onClick={()=> navigate("/appointments")}/>
+          <h2>Randevu</h2>
+        </div>
+        <div className="icon-item">
+          <TbVaccine className="icon" onClick={()=> navigate("/vaccinations")}/>
+          <h2>Aşı</h2>
+        </div>
+        <div className="icon-item">
+          <TbReportMedical className="icon" onClick={()=> navigate("/reports")}/>
+          <h2>Rapor</h2>
+        </div>
+      </div>
+    </div>
+  );
 }
-
 export default Home
